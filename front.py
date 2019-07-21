f0dbe85fe49c15882ac5dc0c52cd9de64555175d
Hugo Da Roit <contact@hdaroit.fr>
2019-07-21 19:57:14 +0200

    Fix front

--- front.py
+++ front.py
@@ -1,26 +1,19 @@
 from flask import Flask
 from flask import render_template
-import cv2
 from tensorflow.python.keras.models import load_model
-from tensorflow.python.keras.preprocessing import image
 import numpy as np
-import matplotlib.pyplot as plt
 from flask import request
+from skimage import transform
+from PIL import Image
 
 app = Flask(__name__)
 
 
-def load_image(img_path, show=False):
-    img = image.load_img(img_path, target_size=(128, 128))
-    img_tensor = image.img_to_array(img)          # (height, width, channels)
-    img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
-    img_tensor /= 255.                                      # imshow expects values in the range [0, 1]
-    if show:
-        plt.imshow(img_tensor[0])
-        plt.axis('off')
-        plt.show()
-
-    return img_tensor
+def load_image(img_path):
+    np_image = Image.open(img_path)
+    np_image = np.array(np_image).astype('float32')
+    np_image = transform.resize(np_image, (128, 128, 3))
+    return np.expand_dims(np_image, axis=0)
 
 
 @app.route("/")
@@ -28,13 +21,13 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/evaluate", methods = ['POST'])
+@app.route("/evaluate", methods=['POST'])
 def evaluate():
-     request_file = request.files['file']
-     model = load_model("./models/project.h5")
-     img = load_image(request_file)
-     pred = model.predict(img)
-     return "This is Pepe the Frog at {:f}%".format(pred[0][1] * 100)
+    request_file = request.files['file']
+    img = load_image(request_file)
+    model = load_model("./models/project.h5")
+    pred = model.predict(img)
+    return "This is Pepe the Frog at {:f}%".format(pred[0][0] * 100)
 
 
 if __name__ == "__main__":
