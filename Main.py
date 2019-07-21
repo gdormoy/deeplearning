ebec74d52a14f67ddcf288c74abee1d771e08dce
dormoy <dormoy.guillaume@gmail.com>
2019-07-21 11:39:37 +0200

    update batch size and remove useless print

--- Main.py
+++ Main.py
@@ -21,10 +21,8 @@ test_data = "./dataset/testing"
 def one_hot_label(img):
     label = img.split(' ')[0]
     if 'pepe' in img:
-        print(img, 'true')
         ohl = np.array([1, 0])
     else:
-        print(img, 'false')
         ohl = np.array([0, 1])
     return ohl
 
@@ -78,9 +76,9 @@ if __name__ == "__main__":
 
     model.compile(optimizer=optimizer,loss='categorical_crossentropy',metrics=['accuracy'])
     model.fit(x=tr_img_data,y=tr_lbl_data,epochs=50,batch_size=100, callbacks=[tb_callback])
-    scores = model.evaluate(x=tst_img_data,y=tst_lbl_data,batch_size=50, callbacks=[tb_callback])
+    score = model.evaluate(x=tst_img_data,y=tst_lbl_data,batch_size=100, callbacks=[tb_callback])
 
-    print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+    print("%s: %.2f%%" % (model.metrics_names[1], score[1]*100))
 
     model.save('./models/project.h5')
     json_string = model.to_json()
